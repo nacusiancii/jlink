52c1d07133fc5c234041010797862ddde38591d9
murali <murali.krishna@nextory.com>
2025-08-22 16:00:59 +0530

    first commit

---
+++ src/main/java/com/nextory/poc/jlink/JlinkApplication.java
@@ -0,0 +1,13 @@
+package com.nextory.poc.jlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JlinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JlinkApplication.class, args);
+	}
+
+}
